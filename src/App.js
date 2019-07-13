3c0b8a366c0c027ac84d58b28840dbedd12afa1d
alex <alex.face@live.com>
2019-07-13 10:06:23 -0400

    handleCount still not working,

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import React, { Component } from "react";
-import CharacterCard from "./components/CharacterCard";
+import React from "react";
+import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import characters from "./characters.json";
 import Header from "./components/Header";
 
-class App extends Component {
-  // Setting this.state.friends to the friends json array
+class App extends React.Component {
   state = {
     characters,
     score: 0,
-    highscore: 0,};
+    highscore: 0
+  };
   
-  handleCount = id => {
-    this.state.characters.find((d, i) => {
-      if (d.id === id) {
-        if(characters[i].wasClicked === 0) {
-          characters[i].wasClicked = characters[i].wasClicked + 1;
-          this.setState({score: this.state.score + 1}, function(){
-            console.log(this.state.score);
-            console.log(characters.wasClicked);      
-          });
-          this.state.characters.sort(() => Math.random() - 0.5)
-          return true;
-        } else {
-          this.gameReset();}}})};
-
   gameReset = () => {
     if (this.state.highscore < this.state.score) {
       this.setState({highscore: this.state.score}, function () {
@@ -35,7 +21,29 @@ class App extends Component {
         character.wasClicked = 0;
       });
       this.setState({score: 0});
-      return true;}
+      return true;
+    }
+  
+  handleCount = id => {
+    this.state.characters.find((d, i) => {
+      if (d.id === id) {
+        if(characters[i].wasClicked === 0) {
+          characters[i].wasClicked = characters[i].wasClicked + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+            console.log(characters.wasClicked);      
+          });
+          this.state.characters.sort(() => Math.random() - 0.5)
+          return true;
+        } else {
+          this.gameReset();
+        }
+      }
+          return true;
+        }
+          );
+        }
+
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
@@ -43,10 +51,9 @@ class App extends Component {
       <Wrapper>
         <Header score={this.state.score} highscore={this.state.highscore}>Memory Click Game</Header>
         {this.state.characters.map(character => (
-          <CharacterCard
+          <Card
             id={character.id}
             key={character.id}
-            name={character.name}
             image={character.image}/>
         ))}
       </Wrapper>);}}
---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Card = props => (
+
+    <div className="card" onClick={() => props.handleCount(props.id)}>
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+    </div>
+  );  
+
+export default Card;
